4a3d7a7741dd0df13b95ad6b2b572aa134b03356
一个老兵 <xiao66guo@163.com>
2017-02-05 14:52:43 +0800

    Initial Commit

---
+++ SQLite的使用/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SQLite的使用
+//
+//  Created by 小果 on 2017/2/5.
+//  Copyright © 2017年 小果. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SQLite的使用/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SQLite的使用
+//
+//  Created by 小果 on 2017/2/5.
+//  Copyright © 2017年 小果. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
